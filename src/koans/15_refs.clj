6cc34e68c44e523ad3ade8c334fb84622ef0667f
worace <horace.d.williams@gmail.com>
2015-03-13 07:48:51 -0600

    koans

--- src/koans/15_refs.clj
+++ src/koans/15_refs.clj
@@ -25,18 +25,18 @@
           @the-world))
 
   "Don't forget to do your work in a transaction!"
-  (= 0 (do __
+  (= 0 (do (dosync (ref-set the-world 0))
            @the-world))
 
   "Functions passed to alter may depend on the data in the ref"
   (= 20 (do
-          (dosync (alter the-world ___))))
+          (dosync (alter the-world (fn [x] (+ x 20))))))
 
   "Two worlds are better than one"
   (= ["Real Jerry" "Bizarro Jerry"]
        (do
          (dosync
           (ref-set the-world {})
-          (alter the-world assoc :jerry "Real Jerry")
+          (alter the-world assoc :pizza "Real Jerry")
           (alter bizarro-world assoc :jerry "Bizarro Jerry")
-          __))))
+          [(get the-world :jerry) (get bizarro-world :jerry)]))))
